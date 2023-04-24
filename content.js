f2f2cadad6c2b719e6d5960089dc1e9eb9e29b41
TaniPotato <tks1610@gmail.com>
2023-04-24 14:05:47 +0900

    first commit

---
+++ content.js
@@ -0,0 +1,24 @@
+// テキストエリア内の改行文字を削除する関数
+function removeLineBreaks() {
+  navigator.clipboard.readText()
+  .then(text => {
+    const withoutNewLine = text.replace(/[\r\n]+/g, "");
+    navigator.clipboard.writeText(withoutNewLine);
+  })
+  .catch(err => {
+    console.error('クリップボードからのテキスト読み取りに失敗しました。', err);
+  });
+}
+
+// ページ内のボタンを作成する関数
+function createButton() {
+  let button = document.createElement('button');
+  button.id="my-button";
+  button.innerText = '改行を削除';
+  button.addEventListener('click', removeLineBreaks);
+  const targetElement = document.getElementById('headlessui-tabs-tab-2');
+  targetElement.parentNode.insertBefore(button, targetElement.nextSibling);
+}
+
+// ページが読み込まれたときにボタンを作成する
+window.addEventListener('load', createButton);
